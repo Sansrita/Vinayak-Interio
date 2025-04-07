2d8ef3e1eb5e8841f4b63d2157818d9be33b14ee
Sansrita <sansritasaha@gmail.com>
2025-04-07 19:35:38 +0530

    Header: added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,37 @@
 'use client';
 
 import React from 'react';
-
+import Logo from './Logo';
 import ThemeToggler from './ThemeToggler';
+import Nav from './Nav';
+import Mobilenavigation from './Mobilenavigation';
 
 const Header = () => {
   return (
-    <div>
-      <ThemeToggler />
-    </div>
+    <header>
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-between'>
+          {/* Logo Section */}
+          <Logo />
+          <div className='flex items-center gap-x-6'>
+            {/* Nav Section*/}
+            <Nav
+            containerStyles='hidden xl:flex gap-x-8 items-center'
+            linkStyles='relative hover:text-primary transition-all'
+            underlineStyles='absolute left-0 top-full h-[2px] bg-primary w-full'
+            />
+            {/* Theme Toggler*/}
+            <ThemeToggler />
+
+            {/* Mobile Navigation */}
+            <div className='xl:hidden'>
+              <Mobilenavigation />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </header>
   )
 }
 
---
+++ components/Mobilenavigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mobilenavigation = () => {
+  return (
+    <div>
+      Mobilenavigation
+    </div>
+  )
+}
+
+export default Mobilenavigation
